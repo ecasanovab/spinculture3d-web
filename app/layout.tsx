9a4d32f30ee809ef6c52986d379e9a1234831e70
Enric <ecasanovab@gmail.com>
2026-06-15 13:06:02 +0200

    Add Analytics component to layou2t

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
